3acf16062a9361ef0483b49500ae70886bbce25b
nishant-moond <60313489+nishant-moond@users.noreply.github.com>
2020-01-27 21:57:00 +0530

    Add files via upload

---
+++ mathfun.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+class mathfun{
+    public static void main(String args[])
+    {
+        double x;
+        x=Math.max(23,33);
+        System.out.println("maximium is:"+x);
+        x=Math.sqrt(32);
+        System.out.println("sqrt is:"+x);
+        x=Math.abs(-55);
+        System.out.println("absolute value:"+x);
+        x=Math.pow(6,3);
+        System.out.println("cube is:"+x);
+        x=Math.min(15,98);
+        System.out.println("minimum is:"+x);
+        x=Math.log(2);
+        System.out.println("log is:"+x);
+    }
+
+}
