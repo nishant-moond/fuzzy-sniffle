4aa8513cb5ae1875b714e33d13bbc62b1878e5ac
nishant-moond <60313489+nishant-moond@users.noreply.github.com>
2020-01-27 21:58:46 +0530

    Add files via upload

---
+++ typecasting.java
@@ -0,0 +1,9 @@
+class typecasting{
+    public static void main(String arg[])
+    {
+        float a=3.4f;
+        int y=(int)a;
+        double d=a;
+        System.out.println("a="+d);
+    }
+}
